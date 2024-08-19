34be9a1f2075b50bd1a1f4d39037bcd37840bd85
Ali-Asjad <aliasjadwork@gmail.com>
2024-08-19 17:51:17 -0400

    first commit

---
+++ app/api/generate/route.js
@@ -0,0 +1,7 @@
+import {NextResponse} from "next/server";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
